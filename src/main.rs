953e4041698c54d2576a30ad0371b96d805ae370
Bradley Horlander <bradley.s.horlander@gmail.com>
2023-02-05 16:25:35 -0600

    numeric literals and try into casting

---
+++ src/main.rs
@@ -0,0 +1,43 @@
+use std::convert::TryInto;
+
+fn main() {
+    let twenty = 20;
+    let twenty_one: i32 = 21;
+    let twenty_two = 22i32;
+
+    let addition = twenty + twenty_one + twenty_two;
+    println!("{} + {} + {} = {}", twenty, twenty_one, twenty_two, addition);
+
+    let one_million: i64 = 1_000_000;
+    println!("{}", one_million.pow(2));
+
+    let forty_twos = [
+        42.0,
+        42f32,
+        42.0_f32,
+    ];
+
+    println!("{:02}", forty_twos[0]);
+
+    //  base 2, base8, and base16 numeric literals
+    let three = 0b11;
+    let thirty = 0o36;
+    let three_hundred = 0x12C;
+
+    println!("base 10: {} {} {}", three, thirty, three_hundred);
+    println!("base 2: {:b} {:b} {:b}", three, thirty, three_hundred);
+    println!("base 8: {:o} {:o} {:o}", three, thirty, three_hundred);
+    println!("base 16: {:x} {:x} {:x}", three, thirty, three_hundred);
+
+
+    //   try_into instead of casting 
+    let a: i32 = 10;
+    let b: u16 = 100;
+
+    let b_ = b.try_into().unwrap();
+
+    if a < b_ {
+        println!("Ten is less than one hundred.");
+    }
+
+}
